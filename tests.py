534e3596c516784f1fc4418ac3c326c50b8fc753
work <adrianzdev@gmail.com>
2019-07-31 22:17:40 +0100

    add User model tests

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 from app import create_app, db
-from app.models import User
+from app.models import User, Run
 from config import Config
 import unittest
 
@@ -10,8 +10,10 @@ class configForTests(Config):
 
 class UserModelCase(unittest.TestCase):
     
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
         self.app = create_app(configForTests)
+        
         
     def test_password(self):
         u = User()
@@ -20,8 +22,25 @@ class UserModelCase(unittest.TestCase):
         u.set_password(correct_pass)
         self.assertTrue(u.check_password(correct_pass))
         self.assertFalse(u.check_password(wrong_pass))
-        
-        
-        
+     
+    
+    def test_new_user(self):
+        with self.app.app_context():
+            u = User(username="mark", email="mark@example.com")
+            db.session.add(u)
+            db.session.commit()
+            get_user = User.query.filter_by(username="mark").first()
+            self.assertEqual(get_user.username, u.username)
+            self.assertEqual(get_user.email, u.email)
+            
+            
+           
+    @classmethod
+    def tearDownClass(self):
+        with self.app.app_context():
+            db.drop_all()
+     
+    
+    
 if __name__ == '__main__':
     unittest.main()
